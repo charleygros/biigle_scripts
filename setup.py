f022a438692bc2c69ef870500eb22b4ead490828
charleygros <charley.gros@gmail.com>
2020-07-23 11:35:38 +1000

    setup: entry_point

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name='biigle_scripts',
       version='1.0',
-      packages=find_packages())
+      packages=find_packages(),
+      entry_points={
+            'console_scripts': [
+                  'convert_laser_circle_to_point=convert_laser_circle_to_point:main'
+            ]}
+      )
