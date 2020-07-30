c02711b6d08e144588cd018a6fe7c7819ad61873
charleygros <charley.gros@gmail.com>
2020-07-30 16:32:22 +1000

    pp: ofolder

--- pull_patches.py
+++ pull_patches.py
@@ -1,4 +1,7 @@
+import os
+import shutil
 import argparse
+import requests
 from biigle.biigle import Api
 
 
@@ -18,9 +21,11 @@ def get_parser():
 
     # OPTIONAL ARGUMENTS
     optional_args = parser.add_argument_group('OPTIONAL ARGUMENTS')
-    optional_args.add_argument('-l', '--label-name', dest='label_name', required=False, type=str,
-                               help='Label name to download. If indicated, only patches from this label are pulled. '
-                                    'Otherwise, all patches are downloaded.')
+    optional_args.add_argument('-l', '--label-id', dest='label_id', required=False, type=str,
+                               help='Label ID to download. If indicated, only patches from this label are pulled. '
+                                    'Otherwise, all patches are downloaded. You can find the ID of a label in the JSON '
+                                    'output of the label tree, eg https://biigle.de/api/v1/label-trees/1, by replacing '
+                                    '"1" by the ID of your label-tree of interest.')
     optional_args.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS,
                                help='Shows function documentation.')
 
@@ -52,14 +57,9 @@ def get_label_annotations(annotation_info, label_name):
     return new_annotation_list
 
 
-def pull_patches(email, token, survey_name, label_name=None):
+def pull_patches(email, token, survey_name, output_folder, label_id=None):
     # Init API
     api = Api(email, token)
-
-    # Get the available annotation shapes.
-    # https://biigle.de/doc/api/index.html#api-Shapes-IndexShapes
-    shapes = api.get('shapes').json()
-    shapes = {s['name']: s['id'] for s in shapes}
 
     # Get all surveys
     surveys = api.get('volumes').json()
@@ -67,15 +67,46 @@ def pull_patches(email, token, survey_name, label_name=None):
     survey_dict = get_survey(surveys, survey_name)
     survey_id = survey_dict['id']
 
+    # Output folder
+    if os.path.isdir(output_folder):
+        print('\nOutput folder already exists: {}.'.format(output_folder))
+    else:
+        print('\nCreating output folder: {}.')
+        os.makedirs(output_folder)
+
+    # Get annotations
+    # TODO: When label_id is None
+    endpoint_url = '{}s/{}/annotations/filter/label/{}'
+    annotations = api.get(endpoint_url.format("volume", survey_id, label_id)).json()
+    print('\nFound {} annotations.'.format(len(annotations)))
+
+    # Init patch URL
+    patch_url = 'https://biigle.de/storage/largo-patches/{}/{}/{}/{}.jpg'
+
+    """
+    for annotation_id, image_uuid in annotations.items():
+        url = patch_url.format(image_uuid[:2], image_uuid[2:4], image_uuid, annotation_id)
+        print('Fetching', url)
+        patch = requests.get(url, stream=True)
+        if patch.ok != True:
+            raise Exception('Failed to fetch {}'.format(url))
+        with open('{}.jpg'.format(annotation_id), 'wb') as f:
+            patch.raw.decode_content = True
+            shutil.copyfileobj(patch.raw, f)
+    """
+    print('\n\n---------- Finished ----------\n\n')
+
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
-    print(args)
+
     # Run function
-    #pull_patches(email=args.email,
-    #             token=args.token,
-    #             survey_name=args.survey_name)
+    pull_patches(email=args.email,
+                 token=args.token,
+                 survey_name=args.survey_name,
+                 output_folder=args.ofolder,
+                 label_id=args.label_id)
 
 
 if __name__ == "__main__":
