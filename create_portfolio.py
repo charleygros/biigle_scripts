d091a9da81943dd0da5774ce64f19f3727b88911
charleygros <charley.gros@gmail.com>
2020-08-21 14:46:51 +1000

    create_portfolio: finish

--- create_portfolio.py
+++ create_portfolio.py
@@ -1,6 +1,7 @@
 import os
+import shutil
 import argparse
-import numpy as np
+import random
 import pandas as pd
 
 
@@ -11,10 +12,10 @@ def get_parser():
     mandatory_args = parser.add_argument_group('MANDATORY ARGUMENTS')
     mandatory_args.add_argument('-i', '--input-folder', required=True, type=str,
                                 help='Database folder.')
-    mandatory_args.add_argument('-c', '--coral-net', required=True, type=str,
-                                help='CoralNet folder suffix.')
-    mandatory_args.add_argument('-b', '--biigle', required=True, type=str,
-                                help='BIIGLE folder suffix.')
+    mandatory_args.add_argument('-s', '--suffix', required=True, type=str,
+                                help='Folder suffix, CoralNet or Biigle.')
+    mandatory_args.add_argument('-p', '--platform', required=True, type=int,
+                                help='Platform: for CoralNet enter 0, for Biigle enter 1.')
     mandatory_args.add_argument('-o', '--ofolder', required=True, type=str,
                                 help='Output folder. Created if does not exist yet.')
 
@@ -24,18 +25,61 @@ def get_parser():
                                help='Shows function documentation.')
     optional_args.add_argument('-t', required=False, type=str, default='coralnet_to_biigle.csv',
                                 help='CoralNet to BIIGLE label translation table.')
-    optional_args.add_argument('-n', required=False, type=str, default=10,
+    optional_args.add_argument('-n', required=False, type=int, default=10,
                                 help='Number of patches per label.')
     return parser
 
 
-def create_portfolio(input_folder, suffix_coralnet, suffix_biigle, n_samples, fname_translation, ofolder):
+def create_portfolio(input_folder, suffix, platform, n_samples, fname_translation, ofolder):
     # Create ofolder if does not exist yet
     if not os.path.isdir(ofolder):
+        print('\nCreating new output folder: {}'.format(ofolder))
         os.makedirs(ofolder)
+    else:
+        print('\nOutput folder already exists: {}'.format(ofolder))
 
     # Translation table between CoralNet and BIIGLE
     df_translation = pd.read_csv(fname_translation)
+    platform = 'BIIGLE' if platform else 'CoralNet'
+    labels_of_interest = df_translation[platform].unique().tolist()
+    # Init dict of filenames
+    dict_fname = {label: [] for label in labels_of_interest}
+    # Create label folders
+    print('\nCreating label sub-folders if do not exist...')
+    for label in labels_of_interest:
+        label_ofolder = os.path.join(ofolder, label)
+        if not os.path.isdir(label_ofolder):
+            os.makedirs(label_ofolder)
+
+    # Loop across surveys
+    print('\nLooking for samples...')
+    for survey_name in os.listdir(input_folder):
+        annotation_folder = os.path.join(input_folder, survey_name, survey_name + suffix)
+        if os.path.isdir(annotation_folder):
+            print('\tFor survey {} ...'.format(survey_name))
+            for label in labels_of_interest:
+                label_folder = os.path.join(annotation_folder, label)
+                if os.path.isdir(label_folder):
+                    for img in os.listdir(label_folder):
+                        if img.endswith(".jpg"):
+                            ifname = os.path.join(label_folder, img)
+                            oimg = survey_name + "_" +img if platform == "BIIGLE" else img
+                            ofname = os.path.join(ofolder, label, oimg)
+                            dict_fname[label].append([ifname, ofname])
+
+    # Feed portfolio
+    print('\nFeeding portfolio...')
+    for label in labels_of_interest:
+        print('\tFor label {} ...'.format(label))
+        candidates = dict_fname[label]
+        if len(candidates) > n_samples:
+            selected = random.sample(candidates, n_samples)
+        else:
+            selected = candidates
+        for ifname, ofname in selected:
+            shutil.copyfile(ifname, ofname)
+
+    print('\n\n---------- Finished ----------\n\n')
 
 
 def main():
@@ -44,8 +88,8 @@ def main():
 
     # Run function
     create_portfolio(input_folder=args.input_folder,
-                     suffix_coralnet=args.coral_net,
-                     suffix_biigle=args.biigle,
+                     suffix=args.suffix,
+                     platform=args.platform,
                      n_samples=args.n,
                      fname_translation=args.t,
                      ofolder=args.ofolder)
